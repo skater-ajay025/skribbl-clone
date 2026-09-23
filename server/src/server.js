25f8d262831110a053a14a5a36cd968998deee31
skater-ajay025 <ajayk59084@gmail.com>
2026-09-24 00:51:29 +0530

    Fix Render server startup

--- server/src/server.js
+++ server/src/server.js
@@ -160,6 +160,6 @@ io.on('connection', socket => {
 });
 const PORT = process.env.PORT || 3000;
 
-server.listen(PORT, "0.0.0.0", () => {
+httpServer.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
-});
+});;
